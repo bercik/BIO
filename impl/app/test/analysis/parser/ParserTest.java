463e3f78b0c31ac4e71852e1c41f6512696ae4cd
Robert <robert.cebula1@gmail.com>
2016-04-27 18:34:55 +0200

    Add parser test

--- impl/app/test/analysis/parser/ParserTest.java
+++ impl/app/test/analysis/parser/ParserTest.java
@@ -35,26 +35,26 @@ import static org.junit.Assert.*;
  */
 public class ParserTest
 {
-    
+
     public ParserTest()
     {
     }
-    
+
     @BeforeClass
     public static void setUpClass()
     {
     }
-    
+
     @AfterClass
     public static void tearDownClass()
     {
     }
-    
+
     @Before
     public void setUp()
     {
     }
-    
+
     @After
     public void tearDown()
     {
@@ -68,20 +68,22 @@ public class ParserTest
             throws Exception
     {
         System.out.println("testNoDefKeywordAtStart");
-        
+
         List<Token<?>> inputTokens = new ArrayList<Token<?>>()
-        {{
-                    add(new Token<>(TokenType.ID, "onSTART", 1, 5));
-                    add(new Token<>(TokenType.OPEN_BRACKET, null, 1, 12));
-                    add(new Token<>(TokenType.CLOSE_BRACKET, null, 1, 13));
-                    add(new Token<>(TokenType.ID, "PRINT", 2, 5));
-                    add(new Token<>(TokenType.OPEN_BRACKET, null, 2, 10));
-                    add(new Token<>(TokenType.STRING, "Hello World!\n", 2, 11));
-                    add(new Token<>(TokenType.CLOSE_BRACKET, null, 2, 27));
-                    add(new Token<>(TokenType.KEYWORD, "end", 3, 1));
-                    add(new Token<>(TokenType.END, null, 4, 1));
-        }};
-        
+        {
+            {
+                add(new Token<>(TokenType.ID, "onSTART", 1, 5));
+                add(new Token<>(TokenType.OPEN_BRACKET, null, 1, 12));
+                add(new Token<>(TokenType.CLOSE_BRACKET, null, 1, 13));
+                add(new Token<>(TokenType.ID, "PRINT", 2, 5));
+                add(new Token<>(TokenType.OPEN_BRACKET, null, 2, 10));
+                add(new Token<>(TokenType.STRING, "Hello World!\n", 2, 11));
+                add(new Token<>(TokenType.CLOSE_BRACKET, null, 2, 27));
+                add(new Token<>(TokenType.KEYWORD, "end", 3, 1));
+                add(new Token<>(TokenType.END, null, 4, 1));
+            }
+        };
+
         boolean catched = false;
         try
         {
@@ -92,35 +94,91 @@ public class ParserTest
             catched = true;
             System.out.println(ex.getMessage());
         }
-        
+
         assertEquals(true, catched);
     }
-    
+
     @Test
     public void testHelloWorld() throws Exception
     {
         System.out.println("testHelloWorld");
-        
+
         List<Token<?>> inputTokens = new ArrayList<Token<?>>()
-        {{
-                    add(new Token<>(TokenType.KEYWORD, "def", 1, 1));
-                    add(new Token<>(TokenType.ID, "onSTART", 1, 5));
-                    add(new Token<>(TokenType.OPEN_BRACKET, null, 1, 12));
-                    add(new Token<>(TokenType.CLOSE_BRACKET, null, 1, 13));
-                    add(new Token<>(TokenType.ID, "PRINT", 2, 5));
-                    add(new Token<>(TokenType.OPEN_BRACKET, null, 2, 10));
-                    add(new Token<>(TokenType.STRING, "Hello World!\n", 2, 11));
-                    add(new Token<>(TokenType.CLOSE_BRACKET, null, 2, 27));
-                    add(new Token<>(TokenType.KEYWORD, "end", 3, 1));
-                    add(new Token<>(TokenType.END, null, 4, 1));
-        }};
-        
+        {
+            {
+                add(new Token<>(TokenType.KEYWORD, "def", 1, 1));
+                add(new Token<>(TokenType.ID, "onSTART", 1, 5));
+                add(new Token<>(TokenType.OPEN_BRACKET, null, 1, 12));
+                add(new Token<>(TokenType.CLOSE_BRACKET, null, 1, 13));
+                add(new Token<>(TokenType.ID, "PRINT", 2, 5));
+                add(new Token<>(TokenType.OPEN_BRACKET, null, 2, 10));
+                add(new Token<>(TokenType.STRING, "Hello World!\n", 2, 11));
+                add(new Token<>(TokenType.CLOSE_BRACKET, null, 2, 27));
+                add(new Token<>(TokenType.KEYWORD, "end", 3, 1));
+                add(new Token<>(TokenType.END, null, 4, 1));
+            }
+        };
+
         List<Integer> expectedSteps = Arrays.asList(0, 3, 5, 9, 12, 13, 15, 19, 17, 11, 2);
-        
+
         Parser parser = new Parser(inputTokens);
-        
         List<Integer> steps = parser.getSteps();
-        
+
+        assertThat(steps, is(expectedSteps));
+    }
+
+    @Test
+    public void testNestedFunctionsAndParams()
+            throws ParserError
+    {
+        System.out.println("testNestedFunctionsAndParams");
+
+        /*
+            def onSTART()
+                CALL2(
+                    PRINT(ADD(2,4)),
+                    PRINT("\nwynik"))
+            end
+         */
+        List<Token<?>> inputTokens = new ArrayList<Token<?>>()
+        {
+            {
+                add(new Token<>(TokenType.KEYWORD, "def", 1, 1));
+                add(new Token<>(TokenType.ID, "onSTART", 1, 5));
+                add(new Token<>(TokenType.OPEN_BRACKET, null, 1, 12));
+                add(new Token<>(TokenType.CLOSE_BRACKET, null, 1, 13));
+
+                add(new Token<>(TokenType.ID, "CALL2", 2, 5));
+                add(new Token<>(TokenType.OPEN_BRACKET, null, 2, 10));
+
+                add(new Token<>(TokenType.ID, "PRINT", 3, 9));
+                add(new Token<>(TokenType.OPEN_BRACKET, null, 3, 14));
+                add(new Token<>(TokenType.ID, "ADD", 3, 15));
+                add(new Token<>(TokenType.OPEN_BRACKET, null, 3, 18));
+                add(new Token<>(TokenType.INT, 2, 3, 19));
+                add(new Token<>(TokenType.COMMA, null, 3, 20));
+                add(new Token<>(TokenType.INT, 4, 3, 21));
+                add(new Token<>(TokenType.CLOSE_BRACKET, null, 3, 22));
+                add(new Token<>(TokenType.CLOSE_BRACKET, null, 3, 23));
+                add(new Token<>(TokenType.COMMA, null, 3, 24));
+
+                add(new Token<>(TokenType.ID, "PRINT", 4, 9));
+                add(new Token<>(TokenType.OPEN_BRACKET, null, 4, 14));
+                add(new Token<>(TokenType.STRING, "\nwynik", 4, 15));
+                add(new Token<>(TokenType.CLOSE_BRACKET, null, 4, 22));
+                add(new Token<>(TokenType.CLOSE_BRACKET, null, 4, 23));
+
+                add(new Token<>(TokenType.KEYWORD, "end", 5, 5));
+                add(new Token<>(TokenType.END, null, 6, 1));
+            }
+        };
+
+        List<Integer> expectedSteps = Arrays.asList(0, 3, 5, 9, 12, 13, 15, 18, 20, 13, 15, 18, 20, 13, 15,
+                19, 16, 15, 19, 17, 17, 16, 15, 18, 20, 13, 15, 19, 17, 17, 11, 2);
+
+        Parser parser = new Parser(inputTokens);
+        List<Integer> steps = parser.getSteps();
+
         assertThat(steps, is(expectedSteps));
     }
 }
