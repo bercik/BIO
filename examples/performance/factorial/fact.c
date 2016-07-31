151c443a0ec71ac33f71d9102fbff1d360f28801
bercik <robert.cebula1@gmail.com>
2016-07-31 10:24:12 +0200

    Add new performance examples

---
+++ examples/performance/factorial/fact.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int factorial(int n)
+{
+   if (n == 1)
+   {
+      return 1;
+   }
+   else
+   {
+      return n * factorial(n - 1);
+   }
+}
+
+int main(void)
+{
+   printf("%d\n", factorial(12));
+}
