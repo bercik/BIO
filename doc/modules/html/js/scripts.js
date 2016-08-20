0079e0a96cf122d6f9cb5e82d078a58bba0804e0
drzazga888 <drzazga888@gmail.com>
2016-08-20 23:53:22 +0200

    doc - blocks can be hided

--- doc/modules/html/js/scripts.js
+++ doc/modules/html/js/scripts.js
@@ -2,7 +2,11 @@ $(document).ready(function() {
 
     var sections = [];
     var nav = $('body').children('nav');
-    var links = nav.find('a');
+    var links = nav.find('li:not(.has-sub)').find('a');
+    var have_subs = nav.find('li.has-sub').find('a');
+    var links_all = links.add(have_subs);
+    var main = $('main');
+    var block_headers = main.find('.block > header, .sub-block > header');
 
     links.each(function() {
         var link = $(this);
@@ -12,7 +16,7 @@ $(document).ready(function() {
         });
     });
 
-    $('main').on('scroll resize', function() {
+    main.on('scroll resize', function() {
         var i = 0;
         while (i < sections.length) {
             var offset = sections[i].main.offset().top;
@@ -20,19 +24,29 @@ $(document).ready(function() {
                 break;
             ++i;
         }
-        links.removeClass('current');
-        sections[i].nav.addClass('current');
+        links_all.removeClass('current');
+        sections[i].nav.parentsUntil(nav, 'li').children('a').addClass('current');
     }).trigger('scroll');
 
     $('#menu-switcher').on('click', function() {
-        if (nav.hasClass('showed'))
-            nav.removeClass('showed');
-        else
-            nav.addClass('showed');
+        nav.toggleClass('showed');
     });
 
     links.on('click', function() {
         nav.removeClass('showed');
+        $($(this).attr('href')).parentsUntil(main, '.block, .sub-block').addClass('showed');
+    });
+
+    have_subs.on('click', function() {
+        $(this).parent().toggleClass('showed');
+    });
+
+    links_all.filter('.function').html(function(_, oldhtml){
+        return oldhtml.replace(/_/g, '_<wbr />');
+    });
+
+    block_headers.on('click', function() {
+        $(this).parent().toggleClass('showed');
     });
 
 });
