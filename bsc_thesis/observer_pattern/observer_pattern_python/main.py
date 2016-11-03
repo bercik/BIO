bf44fcd2f5cb8267badeaa7e9bcd328029e9440c
Robert <robert.cebula1@gmail.com>
2016-11-03 18:04:35 +0100

    Working on bsc_thesis, add chapter 2.

--- bsc_thesis/observer_pattern/observer_pattern_python/main.py
+++ bsc_thesis/observer_pattern/observer_pattern_python/main.py
@@ -17,6 +17,7 @@ def event(func):
 def some_event(data, data2):
     print("some_event " + str(data) + str(data2))
 
+@event
 def foo(data, data2):
     print("foo " + str(data) + str(data2))
 
@@ -24,7 +25,5 @@ def foo2(data, data2):
     print("foo2 " + str(data))
 
 attach_to_event(some_event, foo)
-attach_to_event(some_event, foo2)
-some_event("test", " wiadomosc")
-detach_from_event(some_event, foo)
+attach_to_event(foo, foo2)
 some_event("test", " wiadomosc")
